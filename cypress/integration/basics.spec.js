b887460486a1708720fd837560b7019274329946
Rhenan Medeiros <rhenan.lm@gmail.com>
2020-03-30 12:49:17 -0300

    test e2e - allows users to search for an emoji

--- cypress/integration/basics.spec.js
+++ cypress/integration/basics.spec.js
@@ -10,6 +10,21 @@ describe("2e2 tests", () => {
   });
 
   it("renders the list of emojis", () => {
-    cy.get("[data-cy='emoji-row']").should("have.length", 20)
+    cy.get("[data-cy='emoji-row']").should("have.length", 20);
   });
+
+  it("allows users to search for an emoji", () =>{
+    cy.get("[data-cy='emoji-search-input']").type("joy");
+    cy.get("[data-cy='emoji-row']").should("have.length", 3);
+    cy.contains("Joy");
+    cy.contains("Joy Cat");
+    cy.contains("Joystick");
+
+    cy.get("[data-cy='emoji-search-input']")
+      .clear()
+      .type("cactus");
+    cy.get("[data-cy='emoji-row']").should("have.length", 1);
+    cy.contains("Cactus");
+  });
+
 });
